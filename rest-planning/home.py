86696b381a3f96350bc2519b68d572220f823578
AleitaT <traina@oregonstate.edu>
2018-01-19 08:28:19 -0800

    google app engine server scratch

--- rest-planning/home.py
+++ rest-planning/home.py
@@ -1,16 +1,15 @@
 from google.appengine.ext import ndb
 import webapp2
 
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
-allowed_methods = webapp2.WSGIApplication.allowed.methods
+
+allowed_methods = webapp2.WSGIApplication.allowed_methods
 new_allowed_methods = allowed_methods.union(('PATCH',))
 webapp2.WSGIApplication.allowed_methods = new_allowed_method
-
 app = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
