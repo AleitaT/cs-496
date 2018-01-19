f641c372ede142ea44196080502de0dd0358eb6f
AleitaT <traina@oregonstate.edu>
2018-01-18 20:32:24 -0800

    rest planning

---
+++ rest-planning/home.py
@@ -0,0 +1,16 @@
+from google.appengine.ext import ndb
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+allowed_methods = webapp2.WSGIApplication.allowed.methods
+new_allowed_methods = allowed_methods.union(('PATCH',))
+webapp2.WSGIApplication.allowed_methods = new_allowed_method
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
